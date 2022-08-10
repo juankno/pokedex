da046377cd3f8462cd429a0ce939c7e712a69636
Juan Esteban <juanes0202@gmail.com>
2022-08-10 15:13:40 -0500

    agrwgando la información en el detalle del pokemon

--- src/components/PokemonDetails.tsx
+++ src/components/PokemonDetails.tsx
@@ -2,6 +2,7 @@ import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { PokemonDetail } from '../interfaces/PokemonInterface';
 import { COLORS } from '../theme/constants';
+import { FadeInImage } from './FadeInImage';
 
 interface Props {
     pokemon: PokemonDetail;
@@ -10,11 +11,12 @@ interface Props {
 const PokemonDetails = ({ pokemon }: Props) => {
     return (
         <ScrollView
+            showsVerticalScrollIndicator={false}
             style={{
                 ...StyleSheet.absoluteFillObject,
             }} >
 
-                {/* Types */}
+            {/* Types */}
             <View style={{
                 ...styles.container,
                 marginTop: 370,
@@ -36,15 +38,83 @@ const PokemonDetails = ({ pokemon }: Props) => {
                         ))
                     }
                 </View>
+
+                <Text style={styles.title}>Weight</Text>
+                <Text style={styles.regularText}>{pokemon.weight}Kg</Text>
+
             </View>
 
             {/* Sprites */}
-            <View style={{
-                ...styles.container,
-                marginTop: 20,
-            }}>
+            <View style={styles.container}>
                 <Text style={styles.title}>Sprites</Text>
 
+                <ScrollView
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                >
+                    <FadeInImage
+                        uri={pokemon.sprites.front_default}
+                        style={styles.basicSprite}
+                    />
+
+                    <FadeInImage
+                        uri={pokemon.sprites.back_default}
+                        style={styles.basicSprite}
+                    />
+
+                    <FadeInImage
+                        uri={pokemon.sprites.front_shiny}
+                        style={styles.basicSprite}
+                    />
+
+                    <FadeInImage
+                        uri={pokemon.sprites.back_shiny}
+                        style={styles.basicSprite}
+                    />
+                </ScrollView>
+
+
+            </View>
+
+
+            {/* Habilities */}
+            <View style={styles.container}>
+                <Text style={styles.title}>Habilidades base</Text>
+                <View style={{ flexDirection: 'row' }}>
+                    {
+                        pokemon.abilities.map(({ ability }) => (
+                            <Text
+                                key={ability.name}
+                                style={{
+                                    ...styles.regularText,
+                                    marginRight: 10,
+                                }}
+                            >
+                                {ability.name}
+                            </Text>
+                        ))
+                    }
+                </View>
+
+            </View>
+
+            <View style={styles.container}>
+                <Text style={styles.title}>Movimientos</Text>
+                <View style={{ flexWrap: 'wrap', flexDirection: 'row' }}>
+                    {
+                        pokemon.moves.map(({ move }) => (
+                            <Text
+                                key={move.name}
+                                style={{
+                                    ...styles.regularText,
+                                    marginRight: 10,
+                                }}
+                            >
+                                {move.name}
+                            </Text>
+                        ))
+                    }
+                </View>
 
             </View>
         </ScrollView>
@@ -60,10 +130,16 @@ const styles = StyleSheet.create({
     title: {
         fontWeight: 'bold',
         color: COLORS.black,
-        fontSize: 25,
+        fontSize: 20,
+        marginTop: 20,
     },
     regularText: {
-        fontSize: 20,
+        fontSize: 18,
         color: COLORS.black2,
+    },
+
+    basicSprite: {
+        width: 100,
+        height: 100,
     },
 });
