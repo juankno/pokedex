7612469c153f731bf3d65845443958371f4ef613
Juan Esteban <juanes0202@gmail.com>
2022-08-11 10:21:06 -0500

    diseñando el bottom tab navigator

--- src/navigation/BottomTabNavigator.tsx
+++ src/navigation/BottomTabNavigator.tsx
@@ -4,6 +4,7 @@ import SearchScreen from '../screens/SearchScreen';
 import StackNavigator from './StackNavigator';
 import { COLORS } from '../theme/constants';
 import { Platform } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export type BottomTabParamsList = {
   SearchScreen: undefined;
@@ -20,18 +21,49 @@ const BottomTabNavigator = () => {
       }}
       screenOptions={{
         headerShown: false,
-        tabBarActiveTintColor: COLORS.danger,
+        tabBarActiveTintColor: COLORS.primaryDark,
         tabBarLabelStyle: {
           marginBottom: (Platform.OS === 'ios') ? 0 : 10,
+          color: COLORS.black,
         },
-        tabBarIconStyle: {
-          display: 'none',
+        tabBarStyle: {
+          position: 'absolute',
+          backgroundColor: 'rgba(255, 255, 255, 0.92)',
+          borderWidth: 0,
+          elevation: 0,
+          height: (Platform.OS === 'ios') ? 80 : 60,
         },
       }}
     >
 
-      <Tab.Screen name="StackNavigator" component={StackNavigator} />
-      <Tab.Screen name="SearchScreen" component={SearchScreen} />
+      <Tab.Screen
+        name="StackNavigator"
+        component={StackNavigator}
+        options={{
+          tabBarLabel: 'Listado',
+          tabBarIcon: ({ color }) => (
+            <Icon
+              name="list-outline"
+              size={25}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="SearchScreen"
+        component={SearchScreen}
+        options={{
+          tabBarLabel: 'Buscar',
+          tabBarIcon: ({ color }) => (
+            <Icon
+              name="search-outline"
+              size={25}
+              color={color}
+            />
+          ),
+        }}
+      />
 
     </Tab.Navigator>
   );
